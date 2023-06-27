c6b174e799dfce1015cacde63f6e88c595730835
bkz <bkz@localhost.localdomain>
2023-06-27 14:44:18 -0500

    refactor to use config.ini and fix issues

--- cmp-with-next.lua
+++ cmp-with-next.lua
@@ -8,18 +8,32 @@
 --
 -- NOTE Again, you set how many commits back for each repo you want to
 -- log when you call the fetch_commit_messages function.
+---------------------------------------------------------------------------------
 
--- !!! SET YOUR CONFIG PARAMS IN SECTION BELOW
----------------------------------------------------------------------------------
--- TODO set driver name, E.g. rtw88, rtw89, rtw...
-local driver = "drivername"
-local config = {
-    -- TODO set where wireless-next repo is
-    wireless_next_repo = "/abs/path/to/wireless-next",
-    -- TODO set the base path where you keep standalone driver repos
-    driver_repo = string.format("/abs/path/to/drivers/%s", driver)
+local script_dir = debug.getinfo(1).source:match("@?(.*/)")
+local config_file = script_dir .. 'config.ini'
+
+if not os.execute("test -f " .. config_file) then
+    error("\27[31m" .. [[
+
+The configuration file does not exist.
+Please copy config.ini.example to config.ini and modify it with the proper paths:
+1. Path to the wireless-next repository
+2. Base directory containing standalone drivers.
+        ]] .. "\27[0m")
+end
+
+package.path = package.path .. ";" .. script_dir .. "/lib/?.lua"
+
+local ini = require('inifile')
+local config = ini.parse(config_file)
+
+local driver = arg[1] or "rtw89"
+
+local repos = {
+    wireless_next_repo = config.repositories.wireless_next_repo,
+    driver_repo = config.repositories.driver_repo_base .. "/" .. driver
 }
----------------------------------------------------------------------------------
 
 -- table to store commit messages
 local commit_messages = {
@@ -30,17 +44,27 @@ local commit_messages = {
 -- function to fetch and store commit messages
 function fetch_commit_messages(repo_name, number_of_commits)
     local temp_file = "/tmp/" .. repo_name .. "_commit_messages.txt"
-    os.execute(string.format("cd %s && git log --grep=\"wifi: \"%s --pretty=format:\"%%H %%s\" -n %d > %s",
-                             config[repo_name], driver, number_of_commits, temp_file))
+    os.execute(string.format("cd %s && git log --grep=\"wifi: ".. driver .."\" --pretty=format:\"%%H %%s\" -n %d > %s",
+                             repos[repo_name], number_of_commits, temp_file))
     local file = io.open(temp_file, "r")
     local count = 0
     for line in file:lines() do
         local commit_hash, commit_msg = line:match("([^%s]+)%s(.*)")
-        count = count + 1
-        commit_messages[repo_name][commit_msg] = {hash=commit_hash, order=count}
+
+        -- ensures "Merge" is not the first word in commit_msg
+        if not commit_msg:match("^Merge") then
+            --
+            -- our --grep=\"wifi: ".. driver .." logic above is matching
+            -- on strings in the Merge commit body, thus it is not able
+            -- to filter out merge commits that we don't need
+            --
+            count = count + 1
+            commit_messages[repo_name][commit_msg] = {hash=commit_hash, order=count}
+        end
     end
     file:close()
 end
+
 
 function is_in(commit_msg, commit_table)
     return commit_table[commit_msg] ~= nil
@@ -69,15 +93,27 @@ function create_patches(commit_msg_hash_table, patch_dir)
         local patch_file = string.format("%s/%04d-%s-%s.patch",
                                          patch_dir, data.order, sanitized_msg, data.hash)
         os.execute(string.format("cd %s && git format-patch -1 %s --stdout > %s",
-                                 config['wireless_next_repo'], data.hash, patch_file))
+                                 repos['wireless_next_repo'], data.hash, patch_file))
     end
 end
 
+--
+--                              ███
+--                             ░░░
+--   █████████████    ██████   ████  ████████
+--  ░░███░░███░░███  ░░░░░███ ░░███ ░░███░░███
+--   ░███ ░███ ░███   ███████  ░███  ░███ ░███
+--   ░███ ░███ ░███  ███░░███  ░███  ░███ ░███
+--   █████░███ █████░░████████ █████ ████ █████
+--  ░░░░░ ░░░ ░░░░░  ░░░░░░░░ ░░░░░ ░░░░ ░░░░░
+--
+--  functions, helpers, imports, config, etc. defined above this
+--
+--  main program logic below
+--
 
--- NOTE below is the 'main' logic of this script
-
-fetch_commit_messages('wireless_next_repo', 100)
-fetch_commit_messages('driver_repo', 200)
+fetch_commit_messages("wireless_next_repo", 100)
+fetch_commit_messages("driver_repo", 200)
 
 -- table to store commit hashes that need patches
 local commits_for_patches = {}
@@ -89,5 +125,5 @@ for commit_msg, data in pairs(commit_messages['wireless_next_repo']) do
 end
 
 -- create a patch directory within standalone driver repository
-local patch_dir = config['driver_repo'] .. "/patches"
+local patch_dir = repos['driver_repo'] .. "/patches"
 create_patches(commits_for_patches, patch_dir)
