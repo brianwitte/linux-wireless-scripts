cb97914eb44cef8be7614eac926bab6b271933aa
Brian Witte <13672058+brianwitte@users.noreply.github.com>
2023-06-26 22:39:50 -0500

    change color for device output

--- wi-status.sh
+++ wi-status.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Define some colors for output
-YELLOW='\033[1;33m'
+WHITE='\e[0;37m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 # Check for PCIe Wireless cards
 echo -e "${GREEN}Checking for PCIe Wireless cards...${NC}"
-echo -e "${YELLOW}"
+echo -e "${WHITE}"
 lspci | grep -i wireless
 echo -e "${NC}"
 
 # Check for USB Wireless cards
 echo -e "${GREEN}Checking for USB Wireless cards...${NC}"
-echo -e "${YELLOW}"
+echo -e "${WHITE}"
 lsusb | grep -i nic
 echo -e "${NC}"
 
@@ -21,7 +21,7 @@ echo -e "${NC}"
 echo -e "${GREEN}Checking network configuration...${NC}"
 
 # List all network interfaces
-echo -e "${YELLOW}"
+echo -e "${WHITE}"
 ip link show
 echo -e "${NC}"
 
@@ -29,7 +29,7 @@ echo -e "${NC}"
 for iface in $(ls /sys/class/net); do
     if iwconfig $iface 2>/dev/null | grep -q "ESSID"; then
         echo -e "${GREEN}Wireless details for interface ${iface}${NC}"
-        echo -e "${YELLOW}"
+        echo -e "${WHITE}"
         iwconfig $iface
         echo -e "${NC}"
     fi
